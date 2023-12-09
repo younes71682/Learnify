4f8222cbcd60f1a268cd335e1bee1af9e1bec937
younes <younes71682@gmail.com>
2023-12-09 11:46:18 +0330

    sync

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -6,10 +6,13 @@ const index = () => {
   return (
     <div className='flex gap-6'>
       <Sidbar />
-      <div className='flex flex-col w-[1070px] bg-'>
+
+      <div className='flex flex-col items-center w-[71%] bg-blue-500'>
+
+
         <div className='flex flex-col bg-red-500'>
           <div className='flex items-center text-lg'>
-            <img src='/icon/user/account/info_user/user.svg' />
+            <img src='/icon/user/account/info_user/user.svg'/>
             <h2>اطلاعات کاربری</h2>
           </div>
           <div className='flex flex-wrap '>
@@ -20,6 +23,8 @@ const index = () => {
             <input placeholder='ddd' className='border-solid border rounded-[10px] w-[28%] h-[48px]' required />
           </div>
         </div>
+
+        
       </div>
     </div>
   );
