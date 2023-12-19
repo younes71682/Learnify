7546f6706fef474063cb97753d9eb4e87fee4c35
javid <javid10sh@gmail.com>
2023-12-19 18:44:00 +0330

    stepper added

--- pages/modares/teaching/index.tsx
+++ pages/modares/teaching/index.tsx
@@ -15,6 +15,12 @@ export const Teaching = () => {
 
     const [activeStep, setActiveStep] = useState(0)
 
+    const [isLoginModalOpen, setLoginModalOpen] = useState(false);
+
+    const handleCloseModule = () => {
+        setActiveStep(3)
+    }
+
     const steps = [
         ' 1 ',
         ' 2 ',
@@ -61,7 +67,29 @@ export const Teaching = () => {
             {activeStep === 3 ? (<div className=' justify-center flex'>
                 <p className='w-[380px] text-sm '>پرداخت و ثبت سفارش، به منزله مطالعه و پذیرفتن قوانین و مقررات استفاده از خدمات لرنیفای است .</p>
             </div>) : (null)}
-
+            {activeStep === 4 ? (
+                < div className="fixed  inset-0 flex z-20 items-center justify-center bg-gray-800 bg-opacity-50">
+                    <div className="bg-white text-center w-[491px] h-[499px] pt-10 rounded-2xl shadow-lg">
+                        <img
+                            style={{
+                                width: '200px',
+                                display: 'block',
+                                WebkitUserSelect: 'none',
+                                margin: 'auto',
+                                backgroundColor: 'hsl(0, 0%, 100%)',
+                            }}
+                            src="https://s3-alpha-sig.figma.com/img/252e/81f9/108f022c0c40a33f17ef0f008672f648?Expires=1704067200&amp;Signature=j0LY83RBW-iggBDgRO4MFr3MV58BSAq9qYCw27Ao33eEtXUKsbkXJN6AxAdEek3l48-jbnH5IJ6~f4mojdbjdAqUXflE6OGVNpXdnxidXwX1qsTc8n~LQwQoH2bEInURYhWC-1U7Oa5YodfpzJt7f~NDkAPlRqxiOl9zUXDFk0YQ3SkBGYBC3OOJoB-3B-rj6Vj82w-4ZYr44zTi~5iIX6Q7JbISJb4nQzFoJkcqWIT9ZP0ODKcJoAstjrDRUorvRQQiOXIBHaSoYrDtC~WHJ~dGMiyWt1-ZN-H2M9gn2FpyZZPptwzJYOSPA0LFEtE5p0oRPmu-K43hU-1KGjOCUw__&amp;Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+                        />
+                        <p className='text-2xl font-bold'>دوره شما با موفقیت ثبت شد</p>
+                        <p className='mt-2'>و پس از بررسی ظرف 3روز آینده منتشر میشود</p>
+                        <button className='border-2 p-3 px-6 mt-10 rounded-xl text-[#00D200] text-lg border-[#00D200]'>ورود به پنل کاربری</button>
+                        <div>
+                            <button onClick={handleCloseModule} className="">x</button>
+                        </div>
+                    </div>
+                </div>
+            ) : (null)
+            }
         </div >
     )
 }
